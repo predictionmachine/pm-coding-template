2de6db5566ed0f4ac01c91635f1561c57476828a
Timothy Chklovski <tchklovski@gmail.com>
2021-05-13 05:39:52 -0700

    Tweak copyright, docstrings

--- projectname/example.py
+++ projectname/example.py
@@ -1,17 +1,22 @@
-"""
-Copyright (C) Prediction Machine Advisers, LLC - All Rights Reserved
-Unauthorized copying of this file, via any medium is strictly prohibited
-Proprietary and confidential
-Written by staff at Prediction Machine, January 2021
+# Copyright (C) Prediction Machine Advisers, LLC - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# Written by staff at Prediction Machine, May 2021
 
-This file is an example of what a python file can look like
+"""
+This file deals with UserId
+
+Provides support for listifying it, eg in service of having thigns as lists
 """
 from typing import List, NewType
 
 # Use NewType to create meaningful distinct types, per https://docs.python.org/3/library/typing.html
 UserId = NewType("UserId", int)
+
+
 some_id = UserId(524313)
 
 
-def foo(user_id: UserId) -> List[UserId]:
+def listify(user_id: UserId) -> List[UserId]:
+    """Returns a list containing the single passed in UserId"""
     return [user_id]
